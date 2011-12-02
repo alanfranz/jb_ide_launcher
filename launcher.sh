e2bef0f2852aa3e79d35524361fc3ae8c0fd277e
Alan Franzoni <username@franzoni.eu>
2011-12-02 17:33:58 +0100

    Fix: better handling for missing files/variables.

--- launcher.sh
+++ launcher.sh
@@ -2,13 +2,21 @@
 LAUNCHER_FILE=$1
 CLASS_NAME=$2
 
+trap "Error while executing script, aborted." ERR
+
 FULL_LAUNCHER_PATH=$(readlink -f $(which ${LAUNCHER_FILE}))
+if [ ! -e "${FULL_LAUNCHER_PATH}" ]
+then
+	echo "Could not find $LAUNCHER_FILE, please specify its full path or make it available in PATH."
+	exit 1
+fi
+
 FULL_LAUNCHER_DIR=$(dirname ${FULL_LAUNCHER_PATH})
 THIS_DIR=$(dirname $(readlink -f $0))
 
 if [ ! -e ${FULL_LAUNCHER_DIR}/agent.jar ] 
 then
-	cp ${THIS_DIR}/agent.jar ${FULL_LAUNCHER_DIR}
+	cp ${THIS_DIR}/agent.jar "${FULL_LAUNCHER_DIR}" 
 fi
 
 cd $FULL_LAUNCHER_DIR
